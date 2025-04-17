8e510a82b9a7f18747c7360200624db11dbe6891
Atomico333 <olakace153@gmail.com>
2025-04-17 15:16:00 -0300

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://Atomico333.github.io',
+  base: 'Bookmark-LP', 
 });
